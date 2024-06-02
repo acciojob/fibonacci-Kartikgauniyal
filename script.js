1b14525553513861e69f120ad8c8517e81f3722a
Kartikgauniyal <146377144+Kartikgauniyal@users.noreply.github.com>
2024-06-02 17:48:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function fibonacci(num) {
 // your code here
+	    if (num === 0) return 0;
+    if (num === 1) return 0;
+	
 	let a = 0;
 	let b = 1;
 	let c = a + b;
-	for(let i = 2; i <= num; i++) {
+	for (let i = 2; i <= num; i++) {
 		a = b;
 		b = c;
 		c = a + b;
